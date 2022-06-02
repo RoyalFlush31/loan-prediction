85e42c44479c9c65a32ecab1a6efbca6584fb3b5
RoyalFlush31 <mail@david-brodhagen.de>
2022-06-03 00:33:32 +0200

    Basic Overview

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import json
 
+data = pd.read_csv("LoanPrediction.csv")
+print(f"Datalänge: {len(data)}")
+print(f'Loan Declines: {len(data.Loan_Status.loc[data.Loan_Status == "N"])}')
+print(f'Loan Approvals: {len(data.Loan_Status.loc[data.Loan_Status == "Y"])}')
+print(f'Data Columns: {data.columns}')
+
