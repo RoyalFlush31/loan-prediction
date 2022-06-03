a014adb386367cba149558cd4236f63b1cd36779
RoyalFlush31 <mail@david-brodhagen.de>
2022-06-03 13:26:06 +0200

    Basic Overview

--- main.py
+++ main.py
@@ -7,3 +7,10 @@ print(f'Loan Declines: {len(data.Loan_Status.loc[data.Loan_Status == "N"])}')
 print(f'Loan Approvals: {len(data.Loan_Status.loc[data.Loan_Status == "Y"])}')
 print(f'Data Columns: {data.columns}')
 
+#pdata = pd.pivot_table(data[data.Loan_Status=='Y'], index = 'Gender', values = 'Loan_ID', aggfunc = 'sum')
+#print(pdata)
+print(data.loc[(data.Loan_Status == "Y")&(data.Gender == "Female")])
+
+
+#for i in range(len(data.columns)-1):
+    #print(data[data.columns[i]])
