c5017c62a6192f4ea557531581e9c2f09b7edf9b
RoyalFlush31 <mail@david-brodhagen.de>
2022-07-06 00:48:46 +0200

    23

--- main.py
+++ main.py
@@ -1,19 +1,81 @@
 import pandas as pd
-import json
+from sklearn.utils import resample
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+import numpy as np
 data = pd.read_csv("LoanPrediction.csv")
 
+def sample(data):
+    #sample = resample(data, replace=False, n_samples=200, random_state=0)
+    #data = data.dropna(True)
+    males = data[data.Gender == 'Male']
+    females = data[data.Gender == 'Female']
+    sample = resample(males, n_samples=len(females), replace=False, random_state=0)
+    data = pd.concat([sample, females])
 
-print(f"Length: {len(data)}")
-print(f'Loan Declines: {len(data.loc[data.Loan_Status == "N"])}')
-print(f'Loan Approvals: {len(data.loc[data.Loan_Status == "Y"])}')
-print(f'Column Names: {data.columns}')
-print(f'Column Distinct Values: {data.loc[data.Loan_Status == "Y"].nunique()}')
+    data['Gender'] = np.where(data['Gender'] == 'Female', 1, 0)
+    data['Married'] = np.where(data['Married'] == 'Yes', 1, 0)
+    data['Self_Employed'] = np.where(data['Self_Employed'] == 'Yes', 1, 0)
+    data['Loan_Status'] = np.where(data['Loan_Status'] == 'Y', 1, 0)
+
+    lb_Mar = LabelEncoder()
+    data['Dependents'] = lb_Mar.fit_transform(data['Dependents'])
+    data['Education'] = lb_Mar.fit_transform(data['Education'])
+    data['Property_Area'] = lb_Mar.fit_transform(data['Property_Area'])
+
+    data = data.drop(columns="Loan_ID")
+    data = data.fillna(data.mean())
+
+    return data
+
+def show(data):
+    sns.set(style="ticks")
+    sns.pairplot(data, hue="Loan_Status")
+    #sns.boxplot(data=data, orient="v", palette="Set2")
+    plt.show()
+
+def analyze(data):
+    print(f"Length: {len(data)}")
+    print(f'Loan Declines | Total {len(data.loc[data.Loan_Status == "N"])}',
+          "| Male ", len(data.loc[(data.Loan_Status == "N") & (data.Gender == "Male")]),
+          "| Female ", len(data.loc[(data.Loan_Status == "N") & (data.Gender == "Female")])
+          )
+    print(f'Loan Approvals | Total {len(data.loc[data.Loan_Status == "Y"])}',
+          "| Male ", len(data.loc[(data.Loan_Status == "Y") & (data.Gender == "Male")]),
+          "| Female ", len(data.loc[(data.Loan_Status == "Y") & (data.Gender == "Female")]),
+          len(data.loc[data.Gender == "Male"]), len(data.loc[data.Gender == "Female"])
+          )
+    print(f'Column Names: {data.columns}')
+    print(f'Column Distinct Values: {data.loc[data.Loan_Status == "Y"].nunique()}')
+
+    show(data)
 
 
-data["Loan_Status"].replace({"Y": 0, "N": 1}, inplace=True)
-sns.set(style="ticks")
-sns.pairplot(data, hue="Loan_Status")
-plt.show()
+def process(data):
+    data = data.select_dtypes(exclude=['object'])
+    # Standardization
+    #data = (data - data.mean()) / data.std()
+    # Normalization
+    data = (data - data.min()) / (data.max() - data.min())
+    #print(data.describe())
+    #sns.set(style="ticks")
+    #sns.pairplot(data, hue="Loan_Status")
+    #plt.show()
+    return data
+
+def partion(data):
+    X = data.drop('Loan_Status', axis=1)
+    Y = data['Loan_Status']
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y,
+                                                        stratify=Y, test_size=0.25, random_state=0)
+
+
+data = process(sample(data))
+print(data)
+#analyze(data)
+#show(data)
+
+
 
